ef98e4831de6b1f56362ba90d27e0786b34333e6
Armatik <s.fomchenkov@yandex.ru>
2023-08-25 11:14:07 +0300

    Исправления ошибки при запуске файла Запуск.sh командой "./" в терминале x2

--- Запуск.sh
+++ Запуск.sh
@@ -2,6 +2,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo "Запуск Алтая! Введите пароль от root:"
 su - -c "cd $SCRIPTPATH/src && python3 main.py"
-cd $scriptdir/src && python3 main.py
+cd $SCRIPTPATH/src && python3 main.py
 exit 0
 
